28ce40cb51fd9da21d58f878fdf510925148dbfe
gitvai <vaibhavsagupta@gmail.com>
2026-02-03 10:50:14 +0530

    Cleanup: remove unused scripts and redundant data files

--- check_pre.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-f = "c:/Users/Vaibhav/Desktop/Dashboard-2/Student data/Student data/pre observation.csv.xlsx"
-df = pd.read_excel(f, header=1)
-print(df.columns.tolist())
-print(df.head(2).to_dict())
